3351eb35ca6e10c8dde0f1713bb85d4460a50b04
ArthurPelican <53336221+ArthurPelican@users.noreply.github.com>
2022-05-08 21:14:34 +0200

    Optimisation
    going from 200k points max to several millions without issues

--- value_of_pi.py
+++ value_of_pi.py
@@ -1,4 +1,3 @@
-from unittest import runner
 import pygame
 import random as rd
 import math
@@ -13,13 +12,11 @@ clock = pygame.time.Clock()
 
 WHITE, BLACK, RED = (255, 255, 255), (0, 0, 0), (255, 0, 0)
 
-def add_point(e,  width, height) :
+def add_point(win, center, radius, width, height) :
     tmp_point = (rd.random() * width, rd.random() * height)
-    e.add(tmp_point)
-    return tmp_point
-
-def verify_point(point, center, radius) :
-    distance_to_center = math.sqrt((point[0] - center[0]) ** 2 + (point[1] - center[1]) ** 2)
+    tmp_pixel = (int(tmp_point[0]), int(tmp_point[1]))
+    pygame.draw.circle(win, RED, (tmp_pixel[0], tmp_pixel[1]), 1)
+    distance_to_center = math.sqrt((tmp_point[0] - center[0]) ** 2 + (tmp_point[1] - center[1]) ** 2)
     if distance_to_center <= radius :
         return True
     return False
@@ -35,6 +32,11 @@ def main() :
     total_points = 0
     points_inside = 0
 
+    WIN.fill(WHITE)
+
+    pygame.draw.circle(WIN, BLACK, center, WIDTH // 2)
+    pygame.draw.circle(WIN, WHITE, center, WIDTH // 2 - 2)
+
     while running :
         clock.tick(FPS)
 
@@ -46,53 +48,35 @@ def main() :
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a] :
-            new_point = add_point(points, WIDTH, HEIGHT)
+            inside_circle = add_point(WIN, center, radius, WIDTH, HEIGHT)
             total_points += 1
-
-            inside_circle = verify_point(new_point, center, radius)
             if inside_circle :
                 points_inside += 1
+
         elif keys[pygame.K_z] :
             for _ in range(10) : 
-                new_point = add_point(points, WIDTH, HEIGHT)
+                inside_circle = add_point(WIN, center, radius, WIDTH, HEIGHT)
                 total_points += 1
-
-                inside_circle = verify_point(new_point, center, radius)
                 if inside_circle :
                     points_inside += 1
     
         elif keys[pygame.K_e] :
             for _ in range(100) : 
-                new_point = add_point(points, WIDTH, HEIGHT)
+                inside_circle = add_point(WIN, center, radius, WIDTH, HEIGHT)
                 total_points += 1
-
-                inside_circle = verify_point(new_point, center, radius)
                 if inside_circle :
                     points_inside += 1
         elif keys[pygame.K_r] :
             for _ in range(1000) : 
-                new_point = add_point(points, WIDTH, HEIGHT)
+                inside_circle = add_point(WIN, center, radius, WIDTH, HEIGHT)
                 total_points += 1
-
-                inside_circle = verify_point(new_point, center, radius)
                 if inside_circle :
                     points_inside += 1
-
-        WIN.fill(WHITE)
-
-        pygame.draw.circle(WIN, BLACK, center, WIDTH // 2)
-        pygame.draw.circle(WIN, WHITE, center, WIDTH // 2 - 2)
-
-        for point in points :
-            x = point[0]
-            y = point[1]
-
-            pygame.draw.circle(WIN, RED, (int(x), int(y)), 1)
         
         if total_points > 0 :
             pi_approximation = points_inside / total_points * 4
 
-        pygame.display.set_caption(f"press A to add a random point, press Z to add 10, E to add 100  |  current pi value : {pi_approximation}  |  {points_inside} points inside the circle out of {total_points}")
+        pygame.display.set_caption(f"press A to add a random point, press Z to add 10, E to add 100  |  current pi value : {str(pi_approximation)[0:8]}  |  {points_inside} points inside the circle out of {total_points}")
 
         pygame.display.update()
 
